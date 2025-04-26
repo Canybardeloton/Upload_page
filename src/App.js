9ce44a2950f6a7fdac0e259f1202ec19ecb3f26a
Canybardeloton <armel.gilibert1@gmail.com>
2025-04-26 16:05:43 +0200

    New page Import + App.js restructured

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import './styles/Reset.css'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import Banner from "./components/Banner"
+import ImportFiles from "./pages/ImportFiles"
+import Body from "./components/Body"
+import Connection from './components/Connection'
+import { useState, useEffect } from 'react';
+
+function App(){
+	const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+	useEffect(() => {
+	  const token = localStorage.getItem('userToken');
+	  if (token) {
+		setIsAuthenticated(true);
+	  }
+	}, []);
+
+	const handleLogin = () => {
+	  setIsAuthenticated(true);
+	};
+	return (
+		<BrowserRouter>
+		  <div className="App">
+			<Banner />
+			<Body />
+			<Routes>
+			  {isAuthenticated ? (
+				<>
+				  <Route path="/import" element={<ImportFiles />} />
+				  <Route path="*" element={<Navigate to="/import" replace />} />
+				</>
+			  ) : (
+				<>
+				  <Route path="/" element={<Connection onLogin={handleLogin} />} />
+				  <Route path="*" element={<Navigate to="/" replace />} />
+				</>
+			  )}
+			</Routes>
+		  </div>
+		</BrowserRouter>
+	);
+}
+
+export default App
---
+++ src/pages/ImportFiles.js
@@ -0,0 +1,5 @@
+import React from "react"
+import "../styles/ImportFiles.css"
+
+
+//Composant à avoir : 2 onglets de tableau de bord et d'import de fichiers
